7036e12435737e7ef219a21008a36a453853b9e0
tomimoure8 <tomimoure8@gmail.com>
2025-02-07 13:04:35 -0300

    Resolucion-EJERCICIO1-temporizadores

---
+++ index.js
@@ -0,0 +1,13 @@
+// Ejercicio 1: Uso de setTimeout - Tema: Temporizadores - Consigna: Crea un programa que imprima mensajes con un retraso específico. - Instrucciones:
+// 1) Usa setTimeout para imprimir "hola jeje" después de 1 segundo.
+// 2) Usa setTimeout para imprimir "mentira, chau" después de 3 segundos.
+// Resolución;
+
+setTimeout (() => {
+    console.log ("Hola jeje");
+}, 1000)
+setTimeout (() => {
+    console.log ("mentira bobo, chau juju");
+}, 2000)
+
+
