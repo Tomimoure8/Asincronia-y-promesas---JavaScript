37397798f0be6243166b969be4d4af48b43df58f
tomimoure8 <tomimoure8@gmail.com>
2025-02-07 13:57:15 -0300

    Resolucion-EJERCICIO2: Uso de setInterval

--- index.js
+++ index.js
@@ -10,4 +10,34 @@ setTimeout (() => {
     console.log ("mentira bobo, chau juju");
 }, 2000)
 
+// Ejercicio 2: Uso de setInterval - Tema: Temporizadores repetitivos Consigna: Crea un programa que imprima mensajes repetidamente con un intervalo específico. Instrucciones:
+// 1) Usa setInterval para imprimir "hola" y "mensaje adicional" cada 3 segundos.
+// 2) Crea una variable llamada contador con el valor 10.
+// 3) Usa setInterval para incrementar la variable contador en 1 cada 2 segundos y detener el intervalo cuando contador alcance el valor de 20. Imprime "el contador se frenó" cuando se detenga.
+// 4) Usa setInterval para incrementar una variable numero en 1 cada segundo y detener el intervalo cuando numero alcance el valor de 20. Imprime "capichi, te corté las alas bro" cuando se detenga.
+// Resolución:
+
+setInterval (() => {
+    console.log ("hola, ¿como estás?")
+}, 3000)
+
+let contador = 10;
+let intervalo = setInterval (() => {
+    console.log (contador);
+    contador++
+    if (contador > 20) {
+        clearInterval (intervalo);
+        console.log ("el contador se freno")
+    }
+}, 1000)
+
+let contador2 = 1;
+let intervalo2 = setInterval (() => {
+    console.log (contador2);
+    contador2++;
+    if (contador2 > 20) {
+        clearInterval (intervalo2);
+        console.log ("capichi, te corté las alas bro");
+    }
+}, 1000)
 
